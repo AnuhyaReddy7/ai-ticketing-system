1868ce52861179e02c64b2e3dfa6c6f676228589
Anuhya <anuhya.r.satty@gmail.com>
2026-04-04 22:08:45 +0530

    resolved conflicts

--- backend/database.py
+++ backend/database.py
@@ -7,10 +7,6 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-<<<<<<< HEAD
-
-=======
->>>>>>> 97e4654 (Fix API config and backend updates)
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
